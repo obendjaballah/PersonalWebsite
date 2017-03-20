9003febe1f4f2ff6a6527873a997340172312ffb
obendjaballah <omarbendjaballah@hotmail.com>
2017-03-20 16:01:04 +0100

    added js function for form validation on empty form.

---
+++ Login.js
@@ -0,0 +1,17 @@
+function checkforblank () {
+
+    if (document.getElementById('Uname').value == "") {
+           	alert('Please enter your First Name');
+            document.getElementById('Fname').style.borderColor = "red";
+            return false; 
+            }
+}
+
+function checkforblank () {
+
+    if (document.getElementById('UserPWD').value == "") {
+           	alert('Please enter your Password');
+            document.getElementById('Fname').style.borderColor = "red";
+            return false; 
+            }
+}
